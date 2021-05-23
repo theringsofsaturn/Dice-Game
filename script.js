26659638a270f32f0c7eb3a45ceef7a11d42bd2c
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-05-23 15:54:14 +0200

    bug fix

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 'use strict';
 
-// Select elements
-const score0El = document.querySelector('#score--o');
+// Selecting elements
+const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+
+// Starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
