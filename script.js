b6070e33cacedd432fd841b2a4ee9b5e498437db
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-05-23 15:38:59 +0200

    reference btnRoll

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const score0El = document.querySelector('#score--o');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
+const btnNew = document.querySelector('.btn--roll');
 
 score0El.textContent = 0;
 score1El.textContent = 0;
