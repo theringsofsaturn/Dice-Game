50ef06cb37af1308beb6badf050e2f4a654d7b0d
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-05-23 16:05:06 +0200

    current score 2 reference

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
+current1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
