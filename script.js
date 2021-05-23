c7ab25354cbd079d4a79707766f7b0b5983b9cc8
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-05-23 15:33:22 +0200

    reference to dice

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 'use strict';
 
+// Select elements
 const score0El = document.querySelector('#score--o');
 const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
 
 score0El.textContent = 0;
 score1El.textContent = 0;
