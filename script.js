d787046996279d2e2060564479c0de21cd1a7ad8
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-05-23 15:50:00 +0200

    displaying dice images randomly

--- script.js
+++ script.js
@@ -17,5 +17,6 @@ btnRoll.addEventListener('click', function () {
   const dice = Math.trunc(Math.random() * 6) + 1;
   // 2. Display dice
   diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
   // 3. Check for rolled 1: if true, switch to next player
 });
