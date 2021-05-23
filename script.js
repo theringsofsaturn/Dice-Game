374aaad69767d8313a2892f02f42083977e42d67
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-05-23 15:38:35 +0200

    reference btnNew

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
 const score0El = document.querySelector('#score--o');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
 
 score0El.textContent = 0;
 score1El.textContent = 0;
