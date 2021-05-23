7b100afaa0d9fc342014952501b346892627480e
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-05-23 15:42:58 +0200

    btn roll event listener with function pseudocode

--- script.js
+++ script.js
@@ -10,3 +10,10 @@ const btnHold = document.querySelector('.btn--hold');
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+// Rolling dice funcionality
+btnRoll.addEventListener('click', function () {
+  // 1. Generating a random dice roll
+  // 2. Display dice
+  // 3. Check for rolled 1: if true, switch to next player
+});
