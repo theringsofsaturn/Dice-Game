cdb26a5c9dfe2de3f7a08cfcd87ea8d8ad3c5e39
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-05-23 15:40:14 +0200

    reference btnHold

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
-
+const btnHold = document.querySelector('.btn--hold');
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
